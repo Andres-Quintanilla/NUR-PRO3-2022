108d2feb01b2df88f56ed5d66f17e8a869685fb7
Andres Quintanilla C <https://github.com/Andres-Quintanilla/NUR-2022.git>
2022-04-07 18:13:22 -0400

    Proyectos pro3-NUR-2022

--- practico2/modelo/paisaje.java
+++ practico2/modelo/paisaje.java
@@ -12,10 +12,10 @@ import java.awt.*;
         public void dibujarPaisaje(Graphics g){
         //Cielo
         g.setColor(Color.cyan);
-        g.fillRect(0,0,650 + tamano,60 + tamano);
+        g.fillRect(0,0,tamano + 650,tamano + 60);
         //suelo
         g.setColor(Color.GREEN);
-        g.fillRect(0,0 + 310,650 + tamano,240 + tamano);
+        g.fillRect(0,0 + 310, tamano + 650,tamano + 240);
         //sol
         g.setColor(Color.yellow);
         g.fillOval(0 + 645,0 + 5,tamano - 50,tamano - 50);
