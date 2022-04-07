92915dd1449ba5cbddc05f7b55eff31a45a6f2e6
Andres Quintanilla C <https://github.com/Andres-Quintanilla/NUR-2022.git>
2022-04-07 19:19:33 -0400

    Proyectos pro3-NUR-2022

--- practico2/modelo/arbol.java
+++ practico2/modelo/arbol.java
@@ -60,49 +60,37 @@ public class arbol {
     }
 
     private void  dibujarArbol(Graphics g, int tamano, int angulo, int x, int y, int complejidad){
+
         int xmover = (int)(Math.cos(Math.toRadians(angulo + 90))* tamano);
         int ymover = (int)(Math.sin(Math.toRadians(angulo - 90))* tamano);
+
         g.setColor(Color.BLACK);
         g.drawLine(x,y,x + xmover,y + ymover);
+
         if (tamano >= 1){
-            dibujarArbol(g, tamano - 20, angulo + 30, x + xmover, y + ymover, complejidad);
-            dibujarArbol(g, tamano - 20, angulo - 30, x + xmover, y + ymover, complejidad);
-
-           /*int newComplejidad = complejidad - 1;
-
-            dibujarArbol(g, tamano - 50, angulo + 30, x + xmover, y + ymover, complejidad);
-            dibujarArbol(g, tamano - 50, angulo - 30, x + xmover, y + ymover, complejidad);
-
-            dibujarArbol(g, tamano - 34, angulo + 30, x + xmover, y + ymover, complejidad);
-            dibujarArbol(g, tamano - 34, angulo - 30, x + xmover, y + ymover, complejidad);
-
-            dibujarArbol(g, tamano - 25, angulo + 30, x + xmover, y + ymover, complejidad);
-            dibujarArbol(g, tamano - 25, angulo - 30, x + xmover, y + ymover, complejidad);
-
-            dibujarArbol(g, tamano - 20, angulo + 30, x + xmover, y + ymover, complejidad);
-            dibujarArbol(g, tamano - 20, angulo - 30, x + xmover, y + ymover, complejidad);*/
+            dibujarArbol(g, tamano - 100, angulo + 30, x + xmover, y + ymover, complejidad);
+            dibujarArbol(g, tamano - 100, angulo - 30, x + xmover, y + ymover, complejidad);
             return;
         }
+        /*int newComplejidad = complejidad - 1;
 
+        dibujarArbol(g, tamano - 100, angulo + 30, x + xmover, y + ymover, complejidad);
+        dibujarArbol(g, tamano - 100, angulo - 30, x + xmover, y + ymover, complejidad);
 
+        dibujarArbol(g, tamano - 50, angulo + 30, x + xmover, y + ymover, newComplejidad);
+        dibujarArbol(g, tamano - 50, angulo - 30, x + xmover, y + ymover, newComplejidad);
+
+        dibujarArbol(g, tamano - 34, angulo + 30, x + xmover, y + ymover, newComplejidad);
+        dibujarArbol(g, tamano - 34, angulo - 30, x + xmover, y + ymover, newComplejidad);
+
+        dibujarArbol(g, tamano - 25, angulo + 30, x + xmover, y + ymover, newComplejidad);
+        dibujarArbol(g, tamano - 25, angulo - 30, x + xmover, y + ymover, newComplejidad);
+
+        dibujarArbol(g, tamano - 20, angulo + 30, x + xmover, y + ymover, newComplejidad);
+        dibujarArbol(g, tamano - 20, angulo - 30, x + xmover, y + ymover, newComplejidad);*/
 
     }
 
-
-
-    /*private void dibujarArbol(Graphics g, int x, int y, int tamano, int complejidad){
-        if(complejidad == 1){
-            g.setColor(Color.BLACK);
-            g.drawLine(x, y, tamano, tamano + 50);
-            return;
-        }
-        int newTamano = tamano / 3;
-        int newComplejidad = complejidad - 1;
-
-        dibujarArbol(g, x,y + newTamano,newTamano, newComplejidad);
-
-
-    }*/
 
     public void addObserver(PropertyChangeListener listener){
         cambios.addPropertyChangeListener(listener);
