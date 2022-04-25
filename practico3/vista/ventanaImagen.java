2d5c8768b906c9c081dfc9e89d026f4277b98ced
Andres Quintanilla C <https://github.com/Andres-Quintanilla/NUR-2022.git>
2022-04-25 17:40:21 -0400

    Proyectos pro3-NUR-2022

--- practico3/vista/ventanaImagen.java
+++ practico3/vista/ventanaImagen.java
@@ -1,6 +1,8 @@
 package practico3.vista;
 
 import practico3.modelo.Imagen;
+import practico3.modelo.transformacion;
+import practico3.modelo.transformarTonoDeGris;
 
 import javax.swing.*;
 import java.awt.*;
@@ -28,6 +30,55 @@ public class ventanaImagen extends JFrame {
         JMenu mnu = new JMenu("Archivo");
         bar.add(mnu);
         JMenuItem item = new JMenuItem("Añadir imagen");
+        JMenuItem rojo = new JMenuItem("Imagen con tonos de rojo");
+        JMenuItem verde = new JMenuItem("Imagen con tonos de verde");
+        JMenuItem azul = new JMenuItem("Imagen con tonos de azul");
+        JMenuItem gris = new JMenuItem("Imagen con tonos de gris");
+        JMenuItem blancoYNegro = new JMenuItem("Imagen con tonos de blanco y negro");
+        JMenuItem espejoHorizontal = new JMenuItem("Imagen en modo espejo horizontal");
+        JMenuItem espejoVertical = new JMenuItem("Imagen en modo espejo vertical");
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnMostrarImagen_clicked();
+            }
+        });
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnMostrarImagen_clicked();
+            }
+        });
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnMostrarImagen_clicked();
+            }
+        });
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnMostrarImagen_clicked();
+            }
+        });
+        gris.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnGris_clicked();
+            }
+        });
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnMostrarImagen_clicked();
+            }
+        });
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnMostrarImagen_clicked();
+            }
+        });
         item.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -36,11 +87,23 @@ public class ventanaImagen extends JFrame {
         });
 
         mnu.add(item);
+        mnu.add(rojo);
+        mnu.add(verde);
+        mnu.add(azul);
+        mnu.add(gris);
+        mnu.add(blancoYNegro);
+        mnu.add(espejoHorizontal);
+        mnu.add(espejoVertical);
         this.setJMenuBar(bar);
         this.pack();
     }
 
     private void btnMostrarImagen_clicked(){
         modelo.leerImagen("c:/imagenes/paisaje.jpg");
+    }
+
+    private void btnGris_clicked() {
+        transformacion tonosDeGris = new transformarTonoDeGris(modelo);
+        tonosDeGris.transformar();
     }
 }
