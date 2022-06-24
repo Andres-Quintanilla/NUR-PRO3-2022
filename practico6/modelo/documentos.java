81662cd05f8729eb9d10a1ba00edd1086a6dd537
Andres Quintanilla C <https://github.com/Andres-Quintanilla/NUR-2022.git>
2022-06-23 20:20:57 -0400

    Proyectos pro3-NUR-2022

--- practico6/modelo/documentos.java
+++ practico6/modelo/documentos.java
@@ -21,4 +21,36 @@ public class documentos implements identificador {
         this.tamanoDelArchivo = tamanoDelArchivo;
         this.nombreOriginalDelArchivo = nombreOriginalDelArchivo;
     }
+
+    public String getNombreNuevoArchivo() {
+        return nombreNuevoArchivo;
+    }
+
+    public void setNombreNuevoArchivo(String nombreNuevoArchivo) {
+        this.nombreNuevoArchivo = nombreNuevoArchivo;
+    }
+
+    public String getTipoDelArchivo() {
+        return tipoDelArchivo;
+    }
+
+    public void setTipoDelArchivo(String tipoDelArchivo) {
+        this.tipoDelArchivo = tipoDelArchivo;
+    }
+
+    public int getTamanoDelArchivo() {
+        return tamanoDelArchivo;
+    }
+
+    public void setTamanoDelArchivo(int tamanoDelArchivo) {
+        this.tamanoDelArchivo = tamanoDelArchivo;
+    }
+
+    public String getNombreOriginalDelArchivo() {
+        return nombreOriginalDelArchivo;
+    }
+
+    public void setNombreOriginalDelArchivo(String nombreOriginalDelArchivo) {
+        this.nombreOriginalDelArchivo = nombreOriginalDelArchivo;
+    }
 }
